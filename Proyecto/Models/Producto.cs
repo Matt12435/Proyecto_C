c7d5784d9c811eaea91f166fc50e8e3e46a379f5
Matt12435 <137357767+Matt12435@users.noreply.github.com>
2023-07-04 12:43:54 -0300

    Agregar archivos de proyecto.

---
+++ Proyecto/Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.Models
+{
+    internal class Producto
+    {
+        private int Id;
+        private string Descripsion;
+        private double Costo;
+        private double PrecioVenta;
+        private int StocK;
+        private int IdUsuario;
+    }
+}
---
+++ Proyecto/Models/Venta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.Models
+{
+    internal class Venta
+    {
+        private int Id;
+        private int IdUsuario;
+        private string Comentario;
+    }
+}
